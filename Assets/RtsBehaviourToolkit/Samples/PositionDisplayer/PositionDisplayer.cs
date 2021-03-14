a6187ef6a297785e1f58427ee7ba2496cd37983d
GilbertNordhammar <gilbertnordhammar@gmail.com>
2021-03-14 20:32:52 +0100

    position marker disappear after a few seconds

--- Assets/RtsBehaviourToolkit/Samples/PositionDisplayer/PositionDisplayer.cs
+++ Assets/RtsBehaviourToolkit/Samples/PositionDisplayer/PositionDisplayer.cs
@@ -8,16 +8,31 @@ public class PositionDisplayer : MonoBehaviour
     // Unity editor
     [SerializeField]
     GameObject _positionMarker;
+    [SerializeField]
+    [Min(0.1f)]
+    float _markerDuration = 1.0f;
 
-    // private
+    // Private
+    GameObject _activePositionMarker;
+    IEnumerator _setMarker;
+
+    IEnumerator SetMarker(Vector3 position)
+    {
+        if (!_activePositionMarker)
+            yield return null;
+
+        _activePositionMarker.transform.position = position;
+        _activePositionMarker.SetActive(true);
+        yield return new WaitForSecondsRealtime(_markerDuration);
+        _activePositionMarker.SetActive(false);
+    }
+
     void HandleCommandGiven(RBTUnitCommander.CommandGivenEvent evnt)
     {
-        if (_positionMarker)
-        {
-            Debug.Log("pos (post): " + evnt.Position);
-            var marker = Instantiate(_positionMarker, evnt.Position, _positionMarker.transform.rotation);
-            marker.transform.position = evnt.Position;
-        }
+        if (_setMarker != null)
+            StopCoroutine(_setMarker);
+        _setMarker = SetMarker(evnt.Position);
+        StartCoroutine(_setMarker);
     }
 
     // Unity functions
@@ -35,7 +50,13 @@ public class PositionDisplayer : MonoBehaviour
 
     void Awake()
     {
-        if (!_positionMarker)
+        if (_positionMarker)
+        {
+            _activePositionMarker = Instantiate(_positionMarker);
+            _activePositionMarker.transform.rotation = _positionMarker.transform.rotation;
+            _activePositionMarker.SetActive(false);
+        }
+        else
             Debug.LogWarning($"Please assign a position marker to PositionDisplayer on '{gameObject.name}'");
     }
 }
