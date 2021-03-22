5e12121dcf2272c3aba1851c6e5d5ca636884385
GilbertNordhammar <gilbertnordhammar@gmail.com>
2021-03-22 21:59:42 +0100

    made unit movement less janky/glitchy

--- Assets/RtsBehaviourToolkit/RBTUnit/RBTUnit.cs
+++ Assets/RtsBehaviourToolkit/RBTUnit/RBTUnit.cs
@@ -95,6 +95,14 @@ namespace RtsBehaviourToolkit
         Vector3 _movementSum;
         CapsuleCollider _collider;
 
+        void SetLookDirection()
+        {
+            if (_rigidBody.velocity.magnitude < 0.1f) return;
+            var lookDirection = _rigidBody.velocity.normalized;
+            lookDirection.y = 0;
+            transform.LookAt(transform.position + lookDirection);
+        }
+
         // Unity functions
         void Awake()
         {
@@ -112,62 +120,36 @@ namespace RtsBehaviourToolkit
             }
             _rigidBody.isKinematic = false;
 
-            // Calculationg adjusted movement (i.e. making it parallell to unit's up vector)
-            var movement = _movementSum.normalized * _speed;
-
-            var diffAngle = Vector3.Angle(transform.up, movement) - 90f;
-            movement = Quaternion.AngleAxis(-diffAngle, transform.right) * movement;
-            var postDiff = Vector3.Angle(transform.up, movement) - 90f;
-
-            _rigidBody.velocity = new Vector3(movement.x, _rigidBody.velocity.y + movement.y, movement.z);
-
-            // Snapping unit to floor
+            // Snapping unit to floor and setting surface normal
+            var surfaceNormal = new Vector3();
             var walkableMask = LayerMask.GetMask(new string[] { "RBT Floor" });
             RaycastHit hit = new RaycastHit();
             if (Physics.Raycast(transform.position + _rigidBody.velocity * Time.fixedDeltaTime, -Vector3.up, out hit, 1, walkableMask))
             {
+                surfaceNormal = hit.normal;
                 _rigidBody.position = hit.point;
+                _rigidBody.position += new Vector3(0, 0.1f, 0);
             }
+
+            SetLookDirection();
+
+            // Calculationg adjusted movement (i.e. making it parallell to unit's up vector)
+            var movement = _movementSum.normalized;
+            var diffAngle = Vector3.Angle(surfaceNormal, movement) - 90f;
+            movement = Quaternion.AngleAxis(-diffAngle, transform.right) * movement;
+            movement *= _speed;
+
+            _rigidBody.velocity = movement;
+
+            // Debug
+            Debug.Log(_rigidBody.velocity.magnitude);
+            Debug.DrawRay(transform.position, movement * 5, Color.red);
+            Debug.DrawRay(transform.position, _movementSum.normalized * 4, Color.white);
+            Debug.DrawRay(transform.position, surfaceNormal * 4, Color.green);
 
             // reset movement until next update
             _movementSum = Vector3.zero;
         }
-
-        private void Update()
-        {
-            if (_rigidBody.velocity.magnitude > 0.1f)
-            {
-                // Have this calculation in FixedUpdate() aswell so that transform.up always gets updated
-                // before movement vector is adjuted;
-                var lookDirection = _rigidBody.velocity.normalized;
-                lookDirection.y = 0;
-                transform.LookAt(transform.position + lookDirection);
-            }
-        }
-
-        /*
-            attempting to make unit snap to surface
-            https://gamedev.stackexchange.com/questions/89693/how-could-i-constrain-player-movement-to-the-surface-of-a-3d-object-using-unity
-        */
-        // void OnCollisionExit(Collision other)
-        // {
-        //     var walkableMask = LayerMask.GetMask(new string[] { "RBT Floor" });
-        //     var otherLayer = other.gameObject.layer;
-
-        //     var leftWalkableSurface = walkableMask == (walkableMask | (1 << otherLayer));
-        //     if (leftWalkableSurface)
-        //     {
-        //         Debug.Log("Left floor");
-        //         RaycastHit hit = new RaycastHit();
-        //         if (Physics.Raycast(transform.position, -Vector3.up, out hit, 100, walkableMask))
-        //         {
-        //             Debug.Log("ray hit");
-        //             var targetRotation = Quaternion.FromToRotation(transform.up, hit.normal);
-        //             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, float.PositiveInfinity);
-        //             _rigidBody.position = hit.point;
-        //         }
-        //     }
-        // }
 
         void OnEnable()
         {
