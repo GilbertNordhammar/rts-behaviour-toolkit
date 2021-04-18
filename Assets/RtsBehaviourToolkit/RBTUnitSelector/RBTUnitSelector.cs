1beca25c61f34f772b259ab4f540216d0c23cca7
GilbertNordhammar <gilbertnordhammar@gmail.com>
2021-04-18 20:16:14 +0200

    support for selecting a single unit by clicking

--- Assets/RtsBehaviourToolkit/RBTUnitSelector/RBTUnitSelector.cs
+++ Assets/RtsBehaviourToolkit/RBTUnitSelector/RBTUnitSelector.cs
@@ -11,17 +11,19 @@ namespace RtsBehaviourToolkit
         [SerializeField]
         Material _material;
 
+        [SerializeField]
+        LayerMask _selectable;
+
         // Public
         public static RBTUnitSelector Instance { get; private set; }
 
         // Private
         Vector2 _selectBoxStartCorner;
         List<RBTUnit> _selectedUnits = new List<RBTUnit>();
+        bool _hasSelectedSingle = false;
 
-        void SelectUnits(SelectBox selectBox)
+        void TrySelectMultipleUnits(SelectBox selectBox)
         {
-            _selectedUnits.Clear();
-
             foreach (var unit in RBTUnit.ActiveUnits)
             {
                 var pointOnScreen = Camera.main.WorldToScreenPoint(unit.Bounds.Center);
@@ -41,12 +43,39 @@ namespace RtsBehaviourToolkit
             }
         }
 
+        void TrySelectSingleUnit()
+        {
+            _selectedUnits.Clear();
+
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var clickMask = RBTConfig.WalkableMask | _selectable;
+            RaycastHit clickHit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out clickHit, 100f, clickMask))
+            {
+                var clickedObject = clickHit.collider.gameObject;
+                var unit = clickedObject.GetComponent<RBTUnit>();
+                if (unit)
+                {
+                    _hasSelectedSingle = true;
+                    _selectedUnits.Add(unit);
+                    unit.Selected = true;
+                }
+            }
+        }
+
+        void DeselectAll()
+        {
+            foreach (var unit in RBTUnit.ActiveUnits)
+                unit.Selected = false;
+        }
+
         // Unity functions
         void Awake()
         {
             if (Instance)
             {
-                Debug.LogWarning($"RBTUnitSelector on '{gameObject.name}' was destroyed as there's already one attached on '{Instance.gameObject.name}'");
+                Debug.LogWarning($"RBTUnitSelector on '{gameObject.name}' was destroyed as there's already one attached on '{name}'");
                 Destroy(this);
                 return;
             }
@@ -54,19 +83,30 @@ namespace RtsBehaviourToolkit
 
             if (!_material)
                 Debug.LogError("Please assign a material in the inspector");
+
+            if (_selectable == 0)
+                Debug.LogWarning($"Please assign layers to 'Selectable' on RBTUnitSelector attached to '{name}'");
         }
 
         void Update()
         {
             if (Input.GetMouseButtonDown(0))
+            {
+                _selectedUnits.Clear();
+                DeselectAll();
                 _selectBoxStartCorner = Input.mousePosition;
+                TrySelectSingleUnit();
+            }
             else if (Input.GetMouseButtonUp(0))
+            {
                 _onUnitsSelected.Invoke(new OnUnitsSelectedEvent(this, _selectedUnits));
+                _hasSelectedSingle = false;
+            }
         }
 
         void OnPostRender()
         {
-            if (!Input.GetMouseButton(0))
+            if (!Input.GetMouseButton(0) || _hasSelectedSingle)
                 return;
 
             var selectBox = new SelectBox(_selectBoxStartCorner, Input.mousePosition);
@@ -74,7 +114,7 @@ namespace RtsBehaviourToolkit
             if (_material)
                 selectBox.Draw(_material);
 
-            SelectUnits(selectBox);
+            TrySelectMultipleUnits(selectBox);
         }
     }
 }
